7bb54224ccfb6410f5861f807cb98c78b32301a8
0xRektified <bugislife@proton.me>
2025-08-23 19:40:01 +0800

    feat: day17 add set storage functionalities

--- basic_storage/programs/basic_storage/src/lib.rs
+++ basic_storage/programs/basic_storage/src/lib.rs
@@ -6,9 +6,37 @@ declare_id!("E2CwkZmjHPHXathgKu2FQjFVt3B1kU9JQmqUMGz7uSPR");
 pub mod basic_storage {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn set(_ctx: Context<Set>, new_x: i64) -> Result<()> {
+        let my_storage = &mut _ctx.accounts.my_storage;
+        my_storage.x = new_x;
+        Ok(())
+    }
+    pub fn print_x(_ctx: Context<PrintX>) -> Result<()> {
+        let x = _ctx.accounts.my_storage.x;
+        msg!("the new value is {}", x);
+        Ok(())
+    }
+
+    pub fn add_one(_ctx: Context<Set>, new_x: i64) -> Result<()> {
+        let my_storage = &mut _ctx.accounts.my_storage;
+        my_storage.x = new_x + 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct PrintX<'info> {
+    pub my_storage: Account<'info, MyStorage>,
+}
+
+#[derive(Accounts)]
+pub struct Set<'info> {
+    #[account(mut, seeds = [], bump)]
+    pub my_storage: Account<'info, MyStorage>,
 }
 
 #[derive(Accounts)]
--- basic_storage/tests/basic_storage.ts
+++ basic_storage/tests/basic_storage.ts
@@ -23,4 +23,44 @@ describe("basic_storage", () => {
         })
         .rpc();
   });
+
+  it("Is set!", async () => {
+    const seeds = [];
+
+    const [myStorage, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+    seeds,
+    program.programId
+    );
+    const tx = await program.methods.set(
+      new anchor.BN(170)
+    )
+    .accounts({
+          myStorage: myStorage,
+    })
+    .rpc();
+
+    console.log(`tx is`, tx);
+
+    await program.methods.addOne(new anchor.BN(170)).accounts({myStorage: myStorage}).rpc();
+    await program.methods.printX().accounts({myStorage: myStorage}).rpc();
+  });
 });
+
+
+/*
+ solana account 8e4cN6Lg7yJr4cpsqLt9tUmp89rphSDpJtH6WTFi4iS5                                                  dev@Gabriels-MBP
+
+Public Key: 8e4cN6Lg7yJr4cpsqLt9tUmp89rphSDpJtH6WTFi4iS5
+Balance: 0.00105792 SOL
+Owner: E2CwkZmjHPHXathgKu2FQjFVt3B1kU9JQmqUMGz7uSPR
+Executable: false
+Rent Epoch: 18446744073709551615
+Length: 24 (0x18) bytes
+0000:   1c f2 3b 85  43 19 31 28  aa 00 00 00  00 00 00 00   ..;.C.1(........
+0010:   00 00 00 00  00 00 00 00
+
+
+The first 8 bytes are the discriminator.
+Our test stored the number 170 in the struct,
+this has a hex representation of aa
+*/
